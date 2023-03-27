bf24046d832c216c19a87331e295b075e1cf28ee
giuseppeiazz98 <iazzetta.giuseppe.5598@gmail.com>
2023-03-27 12:35:14 +0200

    ho creato classe esame

---
+++ esercizio2/src/Esame.java
@@ -0,0 +1,49 @@
+public class Esame {
+    private int matricola;
+    private String cognome;
+    private int voto;
+
+    public Esame(){
+
+    }
+
+    public Esame(int matricola, String cognome, int voto) {
+        this.matricola = matricola;
+        this.cognome = cognome;
+        this.voto = voto;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(int matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getVoto() {
+        return voto;
+    }
+
+    public void setVoto(int voto) {
+        this.voto = voto;
+    }
+
+    @Override
+    public String toString() {
+        return "Esame{" +
+                "matricola=" + matricola +
+                ", cognome='" + cognome + '\'' +
+                ", voto=" + voto +
+                '}';
+    }
+    
+}
